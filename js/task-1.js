efc33215edb39b941a507be622d4cfae93784b1a
Viktor Shadura <shaduravik@gmai.com>
2024-03-23 23:51:28 +0200

    task 3

--- js/task-1.js
+++ js/task-1.js
@@ -64,24 +64,4 @@
 
 // _________________________________
 
-// Функція createArrayOfNumbers(min, max) приймає два параметра:
 
-// min - ціле число, з якого починаються обчислення
-// max - ціле число, до якого включно триватимуть обчислення
-// Доповни код функції createArrayOfNumbers(min, max) таким чином, щоб вона повертала масив усіх цілих чисел від значення min до max включно.
-
-// Оголошена функція createArrayOfNumbers(min, max)
-// Виклик функції createArrayOfNumbers(1, 3) повертає [1, 2, 3]
-// Виклик функції createArrayOfNumbers(14, 17) повертає [14, 15, 16, 17]
-// Виклик функції createArrayOfNumbers(29, 34) повертає [29, 30, 31, 32, 33, 34]
-// Виклик функції createArrayOfNumbers() з випадковими min і max повертає правильний масив
-// В циклі for використовувався метод push
-
-function createArrayOfNumbers(min, max) {
-
-}
-
-console.log(Виклик функції createArrayOfNumbers(1, 3));
-console.log(Виклик функції createArrayOfNumbers(14, 17));
-console.log(Виклик функції createArrayOfNumbers(29, 34));
-console.log(Виклик функції createArrayOfNumbers());
